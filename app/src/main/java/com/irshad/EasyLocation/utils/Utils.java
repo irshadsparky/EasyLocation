f494ed94cc29b19382db58c3989da33786449234
Mohammad Irshad Sheikh <ir9977@gmail.com>
2018-07-14 15:00:14 +0530

    Initial commit

---
+++ app/src/main/java/com/irshad/EasyLocation/utils/Utils.java
@@ -0,0 +1,76 @@
+package com.irshad.EasyLocation.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+
+import java.lang.reflect.InvocationTargetException;
+
+
+
+/**
+ * <pre>
+ *     author: Irshad Sheikh
+ *     time  : 14/7/18
+ * </pre>
+ */
+public final class Utils {
+
+    @SuppressLint("StaticFieldLeak")
+    private static Application sApplication;
+
+    private Utils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Init utils.
+     * <p>Init it in the class of Application.</p>
+     *
+     * @param context context
+     */
+    public static void init(@NonNull final Context context) {
+        init((Application) context.getApplicationContext());
+    }
+
+    /**
+     * Init utils.
+     * <p>Init it in the class of Application.</p>
+     *
+     * @param app application
+     */
+    public static void init(@NonNull final Application app) {
+        Utils.sApplication = app;
+    }
+
+    /**
+     * Return the context of Application object.
+     *
+     * @return the context of Application object
+     */
+    public static Application getApp() {
+        if (sApplication != null) return sApplication;
+        try {
+            @SuppressLint("PrivateApi")
+            Class<?> activityThread = Class.forName("android.app.activityThread");
+            Object at = activityThread.getMethod("currentActivityThread").invoke(null);
+            Object app = activityThread.getMethod("getApplication").invoke(at);
+            if (app == null) {
+                throw new NullPointerException("u should init first");
+            }
+            init((Application) app);
+            return sApplication;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        throw new NullPointerException("u should init first");
+    }
+}
